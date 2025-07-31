273da42b8242c8ad0495b98d3e066b22e33ddce9
EliteApex <svyasabattu@ucsd.edu>
2025-07-31 15:31:46 -0700

    added a dummy function to test

--- code/TBC_Package/dummy.py
+++ code/TBC_Package/dummy.py
@@ -10,3 +10,9 @@ def subtract(x, y):
 def multiply(x, y):
     """Multiply two numbers."""
     return x * y
+
+def divide(x, y):
+    """Divide two numbers."""
+    if y == 0:
+        raise ValueError("Cannot divide by zero.")
+    return x / y
